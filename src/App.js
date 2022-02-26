a35ae3c016e86364cece72b3f8ec41e3c31aec98
metaversepeace <89940958+metaversepeace@users.noreply.github.com>
2022-02-26 09:26:29 -0800

    horrid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import React, { useState } from 'react'
+import Navbar from 'react-bootstrap/Navbar';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+
+import TopBar from './TopBar';
+import Background from './Background';
+import Listings from './Listings';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TopBar/>
+      <Background/>
+      <Listings/>
     </div>
   );
 }
